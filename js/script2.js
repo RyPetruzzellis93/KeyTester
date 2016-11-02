89c12035e0c3dc7f9174f4cd50dded8f845dd36d
Ryan Petruzzellis <jazzbass93@comcast.net>
2016-11-02 10:58:08 -0600

    add pokemon files

---
+++ js/script2.js
@@ -0,0 +1,33 @@
+	
+'use strict';
+
+var pokeTemplate = function(data) {
+	var local = 'http://192.168.1.11/~ryanpetruzzellis/',
+	template = document.querySelector('#template').innerHTML,
+	result = document.querySelector('#result');
+	$.each(data, function(index, value) {
+		result.innerHTML += template
+		.replace( /{{nat}}/, value.Nat)
+		.replace( /{{name}}/, value.Pokemon)
+		.replace( /{{gender}}/, value.Gender)
+		.replace( /{{hp}}/, value.HP)
+		.replace( /{{atk}}/, value.Atk)
+		.replace( /{{def}}/, value.Def)
+		.replace( /{{sa}}/, value.SA)
+		.replace( /{{typei}}/, value.TypeI)
+		.replace( /{{typeii}}/, value.TypeII)
+		.replace( /{{evolvesatby}}/, value.EvolvesAtBy)
+		.replace( /{{evolvesinto}}/, value.EvolvesInto)
+		.replace( /{{image}}/, local+value.Image);
+	});
+}
+
+$.ajax({
+	url: 'data/mydata.json',
+	dataType: 'json',
+	type: 'get',
+	cache: false,
+	success: function(data) {
+		pokeTemplate(data);
+	}
+});
